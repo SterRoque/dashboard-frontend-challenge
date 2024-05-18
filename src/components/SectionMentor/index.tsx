f62365ffafa6c95ffc2dee83d58a000821895662
Sterphane Roque <stermiss2@gmail.com>
2024-05-18 10:48:35 -0300

    chore: section mentor

---
+++ src/components/SectionMentor/index.tsx
@@ -0,0 +1,20 @@
+import { Grid } from '@chakra-ui/react';
+import { MentorProfile } from '../MentorProfileCard';
+import { AboutMentorCard } from '../AboutMentorCard';
+
+export function SectionMentor() {
+   return (
+      <Grid
+         w='100%'
+         h='100%'
+         bgColor='red'
+         templateRows='repeat(2, 1fr)'
+         templateColumns='repeat(2, 1fr)'
+         gap='24px'
+      >
+         <MentorProfile />
+
+         <AboutMentorCard />
+      </Grid>
+   );
+}
