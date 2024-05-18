d76965b9fa84f640b49e96f218a549da8aeae1e3
Sterphane Roque <stermiss2@gmail.com>
2024-05-18 00:31:22 -0300

    feat: header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,74 @@
+import {
+   Box,
+   Flex,
+   Image,
+   Popover,
+   PopoverBody,
+   PopoverCloseButton,
+   PopoverContent,
+   PopoverHeader,
+   PopoverTrigger,
+   Text,
+   Icon,
+} from '@chakra-ui/react';
+import avatarImage from '@assets/images/avatar.png';
+import image from '@assets/images/image.png';
+import { IoChevronDownSharp } from 'react-icons/io5';
+
+export function Header() {
+   return (
+      <Flex
+         bgColor='white'
+         h='88px'
+         align='center'
+         justify='space-between'
+         pr='80px'
+         pl='56px'
+      >
+         <Flex
+            align='center'
+            gap='16px'
+         >
+            <Image
+               src={image}
+               w='50px'
+               h='50px'
+               borderRadius={8}
+            />
+
+            <Popover>
+               <PopoverTrigger>
+                  <Flex
+                     cursor='pointer'
+                     align='center'
+                  >
+                     <Text>Mentoria X</Text>
+                     <Icon
+                        as={IoChevronDownSharp}
+                        w='18px'
+                        h='18px'
+                        ml='25px'
+                     />
+                  </Flex>
+               </PopoverTrigger>
+               <PopoverContent w='250px'>
+                  <PopoverHeader>Confirmation!</PopoverHeader>
+                  <PopoverBody>
+                     Are you sure you want to have that milkshake?
+                  </PopoverBody>
+               </PopoverContent>
+            </Popover>
+         </Flex>
+
+         <Flex>
+            <Flex gap='12px'>
+               <Image src={avatarImage} />
+               <Box>
+                  <Text>Lucilene</Text>
+                  <Text>lucilene@gmail.com</Text>
+               </Box>
+            </Flex>
+         </Flex>
+      </Flex>
+   );
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,16 @@
+import { Header } from '@components/Header';
+import { Container } from '@chakra-ui/react';
+import { SectionMentees } from '@components/SectionMentees';
+
 export function Home() {
    return (
-      <div>
-         <h1>hello world</h1>
-      </div>
+      <Container
+         bgColor='gray.100'
+         h='100vh'
+         maxW='100%'
+      >
+         <Header />
+         <SectionMentees />
+      </Container>
    );
 }
