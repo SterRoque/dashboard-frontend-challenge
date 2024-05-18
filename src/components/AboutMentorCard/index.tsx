c67459be97a8c8636b6362e3253a746494c1ed49
Sterphane Roque <stermiss2@gmail.com>
2024-05-18 10:47:16 -0300

    feat: about mentor card

---
+++ src/components/AboutMentorCard/index.tsx
@@ -0,0 +1,62 @@
+import { Box, Flex, Text } from '@chakra-ui/react';
+import { Button } from '../Button';
+import { FaPen } from 'react-icons/fa';
+import { useState } from 'react';
+
+export function AboutMentorCard() {
+   const [showFullText, setShowFullText] = useState<boolean>(false);
+
+   const toggleShowText = () => {
+      setShowFullText(!showFullText);
+   };
+
+   const text =
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt asperiores assumenda praesentium odit pariatur a, consequuntur enim ad expedita magni odio placeat numquam blanditiis quaerat laborum doloremque explicabo hic possimus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt asperiores assumenda praesentium odit pariatur a, consequuntur enim ad expedita magni odio placeat numquam blanditiis quaerat laborum doloremque explicabo hic possimus!Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt asperiores assumenda praesentium odit pariatur a, consequuntur enim ad expedita magni odio placeat numquam blanditiis quaerat laborum doloremque explicabo hic possimus!';
+
+   const about = showFullText ? text : `${text.substring(0, 380)}...`;
+   const fullAbout = text.length < 380 ? text : about;
+
+   return (
+      <Box
+         p='25px'
+         bgColor='white'
+         borderRadius='16px'
+         h={showFullText ? '100%' : '264px'}
+      >
+         <Flex
+            justify='space-between'
+            align='center'
+         >
+            <Text fontWeight={700}>Sobre</Text>
+            <Button icon={FaPen} />
+         </Flex>
+         <Box>
+            <Text
+               style={
+                  !showFullText && text.length >= 380
+                     ? {
+                          maskImage:
+                             'linear-gradient(to bottom, black 50%, transparent 100%)',
+                       }
+                     : {}
+               }
+            >
+               {fullAbout}
+            </Text>
+
+            {text.length >= 380 && (
+               <Text
+                  onClick={toggleShowText}
+                  variant='link'
+                  colorScheme='blue'
+                  mt={2}
+                  color='indigo.500'
+                  cursor='pointer'
+               >
+                  {showFullText ? 'Ler menos' : 'Ler mais'}
+               </Text>
+            )}
+         </Box>
+      </Box>
+   );
+}
