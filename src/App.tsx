970012fcc0f0900b06d011b5d550c151ca05cd5e
Sterphane Roque <stermiss2@gmail.com>
2024-05-18 10:49:30 -0300

    chore: add chakra custom theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { Home } from '@pages/Home';
+import { theme } from '@constants/theme';
 
 function App() {
    return (
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
          <Home />
       </ChakraProvider>
    );
---
+++ src/constants/theme.ts
@@ -0,0 +1,13 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+   colors: {
+      gray: {
+         50: '#D0D5DD',
+         100: '#f2f4f7',
+      },
+      indigo: {
+         500: '#4963FF',
+      },
+   },
+});
